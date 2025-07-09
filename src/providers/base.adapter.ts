776d6c833ab989441492e1940e1be28c30cf595e
carlkiptoo <carlosiamcarl@gmail.com>
2025-07-09 15:22:57 +0300

    email provider setup

---
+++ src/providers/base.adapter.ts
@@ -0,0 +1,26 @@
+export interface EmailProvider {
+    send(payload: {
+        to: string[];
+        from: string;
+        subject: string;
+        html: string;
+
+    }): Promise<void>;
+}
+
+import {smtpAdapter} from './smtp.adapter';
+import {sesAdapter} from './ses.adapter';
+import {sendgridAdapter} from './sendgrid.adapter';
+
+export const getProviderAdapter = (type: string): EmailProvider => {
+    switch (type) {
+        case 'smtp':
+            return smtpAdapter;
+        case 'ses':
+            return sesAdapter;
+        case 'sendgrid':
+            return sendgridAdapter;
+        default:
+            throw new Error(`Unsupported provider type: ${type}`);
+    }
+}
---
+++ src/services/email.service.ts
@@ -0,0 +1,25 @@
+import { get } from 'http';
+import {getProviderAdapter} from '../providers/base.adapter';
+
+interface SendEmailPayload {
+    to: string[];
+    from: string;
+    subject: string;
+    html: string;
+}
+
+export const sendEmailViaProvider = async ({
+    to,
+    from,
+    subject,
+    html
+}: SendEmailPayload): Promise<void> => {
+    try {
+        const provider = getProviderAdapter(process.env.PROVIDER || 'smtp');
+        await provider.send({to, from, subject, html});
+    } catch(error:  any) {
+        console.error('Failed to send email via provider', error);
+        throw new Error('Failed to send email via provider');
+    }
+
+}
