1becd05390c0f39aa289fe808abfb67526c6ff75
carlkiptoo <carlosiamcarl@gmail.com>
2025-07-09 14:19:14 +0300

    Initial commit

---
+++ src/api/routes/sendEmail.route.ts
@@ -0,0 +1,21 @@
+import {Router} from "express";
+import {sendEmailController} from "../controllers/sendEmail.controller";
+import {validateEmail} from "../middlewares/validateEmail.middleware";
+import {authenticate} from "../middlewares/authenticate.middleware";
+
+const router = Router();
+
+router.post(
+    '/',
+    authenticate,
+    validateEmail,
+    async (req, res, next) => {
+        try {
+            await sendEmailController(req, res);
+        } catch (error) {
+            next(error);
+        }
+    }
+)
+
+export default router;
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import sendEmailRoute from "./api/routes/sendEmail.route";
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(express.json());
+
+
+app.use('v1/send', sendEmailRoute);
+
+app.get('v1/health', (req, res) => {
+    res.status(200).json({status: 'OK', service:'Email Sender'})
+
+});
+
+app.use((err: any, req: any, res: any, next: any) => {
+    console.error(err.stack);
+    res.status(500).json({ message: err.message });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
