cec69fe94e10aba90f03a1bbc07d87231e422b25
carlkiptoo <carlosiamcarl@gmail.com>
2025-07-11 12:27:21 +0300

    Redis backed rate limiting

--- src/api/middlewares/rateLimit.middleware.ts
+++ src/api/middlewares/rateLimit.middleware.ts
@@ -1,6 +1,23 @@
 import rateLimit from 'express-rate-limit';
+import RedisStore from 'rate-limit-redis';
+import {createClient} from 'redis';
+import dotenv from 'dotenv';
+import { send } from 'process';
+
+dotenv.config();
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL || 'redis://redis:6379',
+})
+
+redisClient.connect().catch(console.error);
+
 
 export const rateLimitMiddleware = rateLimit({
+    store: new RedisStore({
+        sendCommand: (...args: string[]) => redisClient.sendCommand(args),
+        prefix: 'email-rate-limit'
+    }),
     windowMs: 15 * 60 * 1000,
     max: 10,
     message: {
